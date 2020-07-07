746ce1bf52e1ee2817ec434d02f5c5900bc6a57e
NoBreads <64139041+NoBreads@users.noreply.github.com>
2020-07-07 21:56:38 +0200

    contact minor fixes

--- Frontend/JS/contactMessage.js
+++ Frontend/JS/contactMessage.js
@@ -1,4 +1,5 @@
 function send() {
-  alert('Ihre Anfrage wird beartbeitet!');
+  alert('Ihre Anfrage wird beartbeitet! Sie werden gleich wieder auf die Hauptseite weitergelitet. Bitte gedulden sie sich.');
+  document.getElementById("anfrage").reset();
   setTimeout("window.location.href='index.html'", 4000);
 }
