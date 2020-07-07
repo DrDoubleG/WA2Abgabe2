09dd3ac98489a65fcb7a5dc27950af993008b70c
NoBreads <64139041+NoBreads@users.noreply.github.com>
2020-07-07 23:45:26 +0200

    search all

--- Backend/dao/searchDao.js
+++ Backend/dao/searchDao.js
@@ -15,13 +15,11 @@ class SearchDao {
         const produktDao = new ProduktDao(this._conn);
         var sql = "SELECT produkt_id FROM tagszuweisung WHERE tags_id=(SELECT ID FROM tags where tags=?)";
         var statement = this._conn.prepare(sql);
-        var result = statement.get(helper.arrayObjectKeysToLower(searchString));
-        var outProducts = [];
+        var result = statement.all(searchString.toLowerCase());
         var length = Object.values(result).length;
-
+        var outProducts = [];
         for(var i=0 ; i<length;i++){
-            
-            outProducts.push(produktDao.loadById(Object.values(result)[i]));
+            outProducts.push(produktDao.loadById(Object.values(Object.values(result)[i])));
         }
         return outProducts;
     }
--- Backend/services/search.js
+++ Backend/services/search.js
@@ -3,12 +3,12 @@ const SearchDao = require("../dao/searchDao.js");
 const express = require("express");
 var serviceRouter = express.Router();
 
-serviceRouter.get("/search/gib/:tag", function(request, response) {
+serviceRouter.get("/search/gib/:id", function(request, response) {
     helper.log("Service Search: Client requested all records with this tag, tag=" + request.params.id);
 
     const searchDao = new SearchDao(request.app.locals.dbConnection);
     try {
-        var result = searchDao.loadProductsByTags(request.params.tag);
+        var result = searchDao.loadProductsByTags(request.params.id);
         helper.log("Service Search: Records by tag loaded");
         response.status(200).json(helper.jsonMsgOK(result));
     } catch (ex) {
