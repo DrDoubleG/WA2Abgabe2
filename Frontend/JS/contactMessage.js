af61aac1fff53e06c10241839b17612819317df6
DrDoubleG <DrDoubleG@users.noreply.github.com>
2020-07-07 22:16:28 +0200

    Daten aktualisieren

--- Frontend/JS/contactMessage.js
+++ Frontend/JS/contactMessage.js
@@ -1,5 +1,4 @@
 function send() {
-  alert('Ihre Anfrage wird beartbeitet! Sie werden gleich wieder auf die Hauptseite weitergelitet. Bitte gedulden sie sich.');
-  document.getElementById("anfrage").reset();
+  alert('Ihre Anfrage wird beartbeitet!');
   setTimeout("window.location.href='index.html'", 4000);
 }
