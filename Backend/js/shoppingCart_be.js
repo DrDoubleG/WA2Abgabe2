277ab2dff266d89d55d0b5e0c8201c1299872b63
DrDoubleG <DrDoubleG@users.noreply.github.com>
2020-07-08 12:35:36 +0200

    Update shoppingCart_be.js

--- Backend/js/shoppingCart_be.js
+++ Backend/js/shoppingCart_be.js
@@ -1,4 +1,4 @@
-async function dynamise(url,menge){
+async function dynamise(url,menge,count){
 	$.ajax({
 			url: url,
 			method: "get",
@@ -9,6 +9,9 @@ async function dynamise(url,menge){
 				console.log(response);
 
 			var obj = response.daten;
+			var numericInput = "InputLoop"+ count;
+			var texti = "price" + count;
+			
 			bruttopreis = (obj.bruttopreis.toString()).replace(".", ",");
 			content += '<div class="row m-5">';
 			content += '<div class="col-1"></div>';
@@ -20,21 +23,32 @@ async function dynamise(url,menge){
 			content += '</div>';
 			content += '<div class="col-1"></div>';
 			content += '<div class="col-2" id="number">';
-			content += '<input step="1" data-step-max="10" class="col-4" type="number" id="inputLoop" value="' + menge + '" data-decimals="0" min="1" max="360" onchange="checkCookie('+obj.id.toString()+');updateShoppingCart(this)" />';
+			content += '<input step="1" data-step-max="10" class="col-4" type="number" id="'+ numericInput +'" value="' + menge + '" data-decimals="0" min="1" max="360" onchange="checkCookie('+obj.id.toString()+');updateShoppingCart(this);priceCalc('+ obj.bruttopreis +',' + String(numericInput) +',' + String(texti) +')" />';
 			content += '</div>';
 			content += '<div class="col-2" id="garbage">';
 			content += '<img src="../pictures/delete.png" class="col-3" alt="Produkt">';
 			content += '</div>';
 			content += '<div class="col-2" id="shopping">';
-			content += '<span>' + bruttopreis + ' €</span>';
+			content += '<span id="'+ texti +'">' + bruttopreis + ' €</span>';
 			content += '</div>';
 			content += '</div>';
-			
+		
 			$('#dyntarget5').html(content);
+			
 });
 };
 
-
+function priceCalc(preis,numericInput1,texti1){
+    let show = document.getElementById(texti1);
+	alert(texti1);
+    document.getElementById(numericInput1).oninput = function () {
+        if(this.value>0){
+        preis=this.value*preis;
+        preis=String(preis.toFixed(2));
+        show.innerHTML = preis.replace(".",",")+"&#x20ac";
+        }
+    }
+}
 
 
 
@@ -42,12 +56,13 @@ $(document).ready( async function () {
     console.log("Document ready, loading data from Service");
 	var warenkorb = JSON.parse(localStorage.getItem('warenkorb'));
 	var content = '';
+	var count;
 	for (i = 0; i < warenkorb.length; i++) {
 		var id = warenkorb[i].Produkt.Id;
 		var menge = warenkorb[i].Menge;
 		var url = "http://localhost:8000/api/produkt/gib/" + id;
-		
-		await dynamise(url,menge);
+		count = i;
+		await dynamise(url,menge, count);
 			
        
 
@@ -74,4 +89,6 @@ function updateShoppingCart(e){
 			
 		
 }
+
+
 
