cda3cd613db1b370ce6148d169fbb428026e8083
NoBreads <64139041+NoBreads@users.noreply.github.com>
2020-07-09 18:13:37 +0200

    allCosts are calculatable shoppingCart

--- Backend/js/shoppingCart_be.js
+++ Backend/js/shoppingCart_be.js
@@ -1,101 +1,145 @@
-async function dynamise(url,menge,count){
+async function dynamise(url, menge, count, warenkorb) {
 	$.ajax({
-			url: url,
-			method: "get",
-			dataType: "json"
-			
-			}).done(function (response) {
-				console.log("Data loaded successfully");
-				console.log(response);
+		url: url,
+		method: "get",
+		dataType: "json"
 
-			var obj = response.daten;
-			var numericInput = "inputLoop"+ count;
-			var texti = "price" + count;
-			
-			bruttopreis = (obj.bruttopreis.toString()).replace(".", ",");
-			content += '<div class="row m-5 onload="priceCalc('+ obj.bruttopreis +',' + numericInput +',' + texti +')">';
-			content += '<div class="col-1"></div>';
-			content += '<div class="col-2">';
-			content += '<a href="product-detail.html" onclick="checkCookie('+obj.id.toString()+')" title="Produktdetails"><img src="' + obj.bilder.bildpfad + '" class="pictures" alt="Produkt"></a>';
-			content += '</div>';
-			content += '<div class="col-2 " id="shopping">';
-			content += '<span>' + obj.name + '</span>';
-			content += '</div>';
-			content += '<div class="col-1"></div>';
-			content += '<div class="col-2" id="number">';
-			content += '<input step="1" data-step-max="10" class="col-4" type="number" id="'+ numericInput +'" value="' + menge + '" data-decimals="0" min="1" max="360" onchange="checkCookie('+obj.id.toString()+');updateShoppingCart(this);priceCalc('+ obj.bruttopreis +',' + numericInput +',' + texti +')" />';
-			content += '</div>';
-			content += '<div class="col-2" id="garbage">';
-			content += '<a onclick="deleteProduct()"><img onclick="checkCookie('+obj.id.toString()+')" src="../pictures/delete.png" class="col-3" alt="Produkt"></a>';
-			content += '</div>';
-			content += '<div class="col-2" id="shopping">';
-			content += '<span id="'+ texti +'">' + bruttopreis + ' €</span>';
-			content += '</div>';
-			content += '</div>';
-		
-			$('#dyntarget5').html(content);
-	
-			
-});
+	}).done(function (response) {
+		console.log("Data loaded successfully");
+		console.log(response);
+		var obj = response.daten;
+		var numericInput = "inputLoop" + count;
+		var texti = "price" + count;
+		var pfandi = "pfand" + count;
+
+		bruttopreis = ((obj.bruttopreis * menge).toFixed(2).toString()).replace(".", ",");
+		content += '<div class="row m-5 onload="priceCalc(' + obj.bruttopreis + ',' + numericInput + ',' + texti + ')">';
+		content += '<div class="col-1"></div>';
+		content += '<div class="col-2">';
+		content += '<a href="product-detail.html" onclick="checkCookie(' + obj.id.toString() + ')" title="Produktdetails"><img src="' + obj.bilder.bildpfad + '" class="pictures" alt="Produkt"></a>';
+		content += '</div>';
+		content += '<div class="col-2 " id="shopping">';
+		content += '<span>' + obj.name + '</span>';
+		content += '</div>';
+		content += '<div class="col-1"></div>';
+		content += '<div class="col-2" id="number">';
+		content += '<input step="1" data-step-max="10" class="col-4" type="number" id="' + numericInput + '" value="' + menge + '" data-decimals="0" min="1" max="360" onchange="checkCookie(' + obj.id.toString() + ');updateShoppingCart(this);priceCalc(' + obj.bruttopreis + ',' + numericInput + ',' + texti + ',' + obj.pfandstufe.gebuehr + ',' + pfandi + ');sumPrices()" />';
+		content += '</div>';
+		content += '<div class="col-2" id="garbage">';
+		content += '<a onclick="deleteProduct()"><img onclick="checkCookie(' + obj.id.toString() + ')" src="../pictures/delete.png" class="col-3" alt="Produkt"></a>';
+		content += '</div>';
+		content += '<div class="col-2" id="shopping">';
+		content += '<span id="' + texti + '">' + bruttopreis + ' €</span>';
+		content += '</div>';
+		content += '</div>';
+		content += '<div id="' + pfandi + '" hidden >' + (obj.pfandstufe.gebuehr * menge) + '</div>'
+
+		$('#dyntarget5').html(content.substring(15, [content.length]));
+
+
+	});
 };
 
 
-function priceCalc(preis,numericInput,texti){
-    let show = texti;
-    numericInput.oninput = function () {
-        if(this.value>0){
-        preis=this.value*preis;
-        preis=String(preis.toFixed(2));
-        show.innerHTML = preis.replace(".",",")+"&#x20ac";
-        }
-    }
+function priceCalc(preis, numericInput, texti, pfand, pfandi) {
+	let show = texti;
+	let notShow = pfandi;
+	numericInput.oninput = function () {
+		if (this.value > 0) {
+			preis = this.value * preis;
+			preis = String(preis.toFixed(2));
+
+			show.innerHTML = preis.replace(".", ",") + "&#x20ac";
+			notShow.innerHTML = this.value * pfand;
+		}
+	}
 }
 
 
 
-$(document).ready( async function () {
-    console.log("Document ready, loading data from Service");
+$(document).ready(async function () {
+	console.log("Document ready, loading data from Service");
 	var warenkorb = JSON.parse(localStorage.getItem('warenkorb'));
-	var content = '';
 	var count;
-	for (i = 0; i < warenkorb.length; i++) {
-		var id = warenkorb[i].Produkt.Id;
-		var menge = warenkorb[i].Menge;
-		var url = "http://localhost:8000/api/produkt/gib/" + id;
-		count = i;
-		await dynamise(url,menge, count);
+	if (warenkorb.length != 0) {
+		for (i = 0; i < warenkorb.length; i++) {
+			var id = warenkorb[i].Produkt.Id;
+			var menge = warenkorb[i].Menge;
+			var url = "http://localhost:8000/api/produkt/gib/" + id;
+			count = i;
+			await dynamise(url, menge, count, warenkorb);
+		}
+		setTimeout(function () {
+			var pfandi = "";
+			var deposit = 0;
+			var interCost = 0;
+			for (j = 0; j < warenkorb.length; j++) {
+				pfandi = "pfand" + j;
+				texti = "price" + j;
+				var pull = document.getElementById(pfandi);
+				var pull2 = document.getElementById(texti);
+				deposit += parseFloat(pull.innerHTML);
+				interCost += parseFloat(pull2.innerHTML);
 			}
-    });
+			document.getElementById("deposit").innerHTML = deposit.toFixed(2) + " €";
+			document.getElementById("interSum").innerHTML = (interCost + deposit).toFixed(2) + " €";
+			document.getElementById("shipping").innerHTML = "20,00 €";
+			document.getElementById("allCost").innerHTML = (deposit + interCost + 20).toFixed(2) + " €";
+
+		}, 3000);
+	}
+});
+
+function sumPrices() {
+	var warenkorb = JSON.parse(localStorage.getItem('warenkorb'));
+	var pfandi = "";
+	var deposit = 0;
+	var interCost = 0;
+	for (j = 0; j < warenkorb.length; j++) {
+		pfandi = "pfand" + j;
+		texti = "price" + j;
+		var pull = document.getElementById(pfandi);
+		var pull2 = document.getElementById(texti);
+		deposit += parseFloat(pull.innerHTML);
+		interCost += parseFloat(pull2.innerHTML);
+	}
+	document.getElementById("deposit").innerHTML = deposit.toFixed(2) + " €";
+	document.getElementById("interSum").innerHTML = (interCost + deposit).toFixed(2) + " €";
+	document.getElementById("shipping").innerHTML = "20,00 €";
+	document.getElementById("allCost").innerHTML = (deposit + interCost + 20).toFixed(2) + " €";
 
 
-function updateShoppingCart(e){
+}
+
+
+function updateShoppingCart(e) {
 	console.log("updateing shopping cart");
 	var warenkorb = JSON.parse(localStorage.getItem('warenkorb'));
 	var menge = e.value;
-			for (i = 0; i < warenkorb.length; i++) {
-                if (warenkorb[i].Produkt.Id == getCookie("objectCookie")) {
-					var erg = parseInt(menge);
-					console.log(warenkorb[i].Menge);
-                    warenkorb[i].Menge = erg;
-					console.log(erg);
-                    break;
-				}
-				
-			}
-			localStorage.setItem('warenkorb', JSON.stringify(warenkorb));
-			
-		
+	for (i = 0; i < warenkorb.length; i++) {
+		if (warenkorb[i].Produkt.Id == getCookie("objectCookie")) {
+			var erg = parseInt(menge);
+			console.log(warenkorb[i].Menge);
+			warenkorb[i].Menge = erg;
+			console.log(erg);
+			break;
+		}
+
+	}
+	localStorage.setItem('warenkorb', JSON.stringify(warenkorb));
+
+
 }
 
 
-function deleteProduct(){
+function deleteProduct() {
 	console.log("delete product");
 	var warenkorb = JSON.parse(localStorage.getItem('warenkorb'));
 
-	for (var i=0; i<warenkorb.length; i++) {
+	for (var i = 0; i < warenkorb.length; i++) {
 		var erg = (warenkorb[i].Produkt.Id);
-		if (erg === getCookie("objectCookie")){
-			warenkorb.splice(i,1);
+		if (erg === getCookie("objectCookie")) {
+			warenkorb.splice(i, 1);
 		}
 	}
 	warenkorb = JSON.stringify(warenkorb);
